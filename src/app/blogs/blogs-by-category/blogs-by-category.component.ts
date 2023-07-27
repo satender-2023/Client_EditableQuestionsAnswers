a7054348068552ed575fea9413563d5012564f2c
satender-2023 <satenderkumar898@gmail.com>
2023-07-27 15:18:21 +0530

    updates

--- src/app/blogs/blogs-by-category/blogs-by-category.component.ts
+++ src/app/blogs/blogs-by-category/blogs-by-category.component.ts
@@ -1,13 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import {SafeResourceUrlPipe} from 'src/app/pipes/safe-resource-url.pipe';
 
 @Component({
   selector: 'app-blogs-by-category',
   templateUrl: './blogs-by-category.component.html',
-  styleUrls: ['./blogs-by-category.component.scss']
+  styleUrls: ['./blogs-by-category.component.scss'],
+  providers: [SafeResourceUrlPipe]
 })
 export class BlogsByCategoryComponent implements OnInit {
+  productImages =[];
+  currentlyUploadedImageSrc = [];
 
-  constructor() { }
+  constructor(
+    private safeResourceUrl: SafeResourceUrlPipe
+    ) { }
+
   urls = [];
   ngOnInit(): void {
   }
@@ -38,4 +45,18 @@ export class BlogsByCategoryComponent implements OnInit {
       }
     }
   }
+
+  setCurrentlyUploadedImgArr(file) {
+    let imgSrc=URL.createObjectURL(file)
+    this.currentlyUploadedImageSrc.push(imgSrc);
+  }
+
+  // getFiles(event) {
+  //   this.productImages=[];
+  //   for (var i = 0; i < event.target.files.length; i++) {
+  //     this.productImages.push(event.target.files[i]);
+  //     this.setCurrentlyUploadedImgArr(event.target.files[i]);
+  //         this.setCurrentlyUploadedImgArr(event.target.files[i])
+  //   }
+  // }
 }
